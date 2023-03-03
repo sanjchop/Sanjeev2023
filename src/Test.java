46f85d9b8e722cd22e1073984681767553ae6061
unknown <sanjeev.chopra@qmmtech.com>
2023-03-03 18:27:24 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args){
+       System.out.println("hello");
+       main();
+    }
+    public static void  main(){
+        System.out.println(" sanjeev");
+    }
+}
